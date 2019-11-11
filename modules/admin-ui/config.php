b0ca70e9ec8bf32c667f806cfeadb0a4e9bb43fa
Iqbal Fauzi <iqbalfawz@gmail.com>
2019-11-11 08:34:47 +0700

    Add form control radio list-group element

--- modules/admin-ui/config.php
+++ modules/admin-ui/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'admin-ui',
-    '__version' => '0.3.0',
+    '__version' => '0.4.0',
     '__git' => 'git@github.com:getmim/admin-ui.git',
     '__license' => 'MIT',
     '__author' => [
