fdf3fcf970d01e08e4365d3958ce00bb9c6b2052
Iqbal Fauzi <iqbalfawz@gmail.com>
2025-05-12 10:25:45 +0700

    Add readonly on input type text

--- modules/admin-ui/config.php
+++ modules/admin-ui/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'admin-ui',
-    '__version' => '0.19.1',
+    '__version' => '0.20.0',
     '__git' => 'git@github.com:getmim/admin-ui.git',
     '__license' => 'MIT',
     '__author' => [
